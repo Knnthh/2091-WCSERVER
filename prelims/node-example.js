37c34fce9e0f76b94f9583545cad8024b8356b88
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-01-23 11:46:17 +0800

    Add files via upload

---
+++ prelims/node-example.js
@@ -0,0 +1,16 @@
+// LACHICA, GIANN KENNETH C.
+// Module 3
+// WEEK 2 AND 3: BASICS OF NODEJS
+
+
+//FIRST PROGRAM
+console.log("Welcome to WCSERVER!!"); 
+
+//BASICS OF NODEJS
+function Display(x) {
+    console.log(x);
+}
+
+Display(200);
+
+var http = require("http:");
