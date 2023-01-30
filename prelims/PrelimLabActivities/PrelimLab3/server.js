3208ffee0c6c1abee8935eb9c9e34dfb98a55221
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-01-30 13:12:21 +0800

    1 file modified

--- prelims/PrelimLabActivities/PrelimLab3/server.js
+++ prelims/PrelimLabActivities/PrelimLab3/server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1></h1>');
-  res.end();
-});
+  if (req.url == '/'){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Welcome to my Node.js Application</h1>');
+    res.end();
 
+  }
 server.listen(5000);
 
 console.log('Node.js web server at port 5000 is running..');
