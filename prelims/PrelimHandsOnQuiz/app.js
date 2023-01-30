34b8b785c0db112e99e1dcf76323b5dc522520b2
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-01-30 23:15:02 +0800

    2 files added

---
+++ prelims/PrelimHandsOnQuiz/app.js
@@ -0,0 +1,6 @@
+const { add, subtract, multiply, divide } = require('./calculation');
+
+console.log(add(10, 10));
+console.log(subtract(10, 10));
+console.log(multiply(10, 10));
+console.log(divide(10, 10));
