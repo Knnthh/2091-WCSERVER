c41fc271dd002c371506848e12aaaf1230ab706a
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-01-30 23:38:27 +0800

    1 file modified

--- prelims/PrelimHandsOnQuiz/app.js
+++ prelims/PrelimHandsOnQuiz/app.js
@@ -1,6 +1,23 @@
 const { add, subtract, multiply, divide } = require('./calculation');
 
-console.log(add(10, 10));
-console.log(subtract(10, 10));
-console.log(multiply(10, 10));
-console.log(divide(10, 10));
+var hrs = 4;
+var day = 6;
+var rate = 300;
+var hours_worked = multiply(hrs, day);
+
+var gross = multiply(hours_worked, rate);
+var tax = multiply(gross, 0.1);
+
+var sss = 1200;
+var pag_ibig = 300;
+var phil_health = 400;
+
+var total_deductions = add(add(sss, pag_ibig), add(tax, phil_health));
+
+console.log('Gross Income: ' + gross);
+console.log('Tax: ' + tax);
+console.log('SSS: ' + sss);
+console.log('Pag-Ibig Fund: ' + pag_ibig);
+console.log('Philhealth: ' + phil_health);
+console.log('Total Deductions: ' + total_deductions);
+console.log('Net Salary: ' + subtract(gross, total_deductions));
