5e634ed9848ebec2d06dc84e136bd900ffc67c57
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-02-08 15:47:48 +0800

    2 files modified

--- prelims/PrelimExam/app.js
+++ prelims/PrelimExam/app.js
@@ -1,5 +1,3 @@
-//Then, you need to create another file called app.js that will use the message module. In the app.js file, you will set a name variable to "your name" and use the functions from the message module to display the messages: "Hello, [Your name here]!", "Congratulations, [Your name here]!", and "Goodbye, [Your name here]!" in the console.
-
 var { greet, congratulations, farewell } = require('./function.js');
 
 console.log(greet('Giann Kenneth Lachica'));
--- prelims/PrelimExam/function.js
+++ prelims/PrelimExam/function.js
@@ -1,19 +1,13 @@
-//Your task is to build a message module with three functions - greet, congratulations, and farewell. The greet function should say hello to someone by printing "Hello, [Name]!" in the console. The congratulations function should print "Congratulations, [Name]!" and the farewell function should print "Goodbye, [Name]!" in the console.
-
-//Finally, test your modules by running the application file using Node.js to see the messages printed in the console.
-
-//Files should be created locally and submission are screenshots of the the codes and sample output.
-
-function greet(name){
-  return "Hello, " + name + "!";
+function greet(name) {
+  return 'Hello, ' + name + '!';
 }
 
-function congratulations(name){
-  return "Congratulations, " + name + "!";
+function congratulations(name) {
+  return 'Congratulations, ' + name + '!';
 }
 
-function farewell(name){
-  return "Goodbye, " + name + "!";
+function farewell(name) {
+  return 'Goodbye, ' + name + '!';
 }
 
 module.exports = { greet, congratulations, farewell };
