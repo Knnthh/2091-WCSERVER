a8c2c6c2f965b36637aba2d69fd9cb4bc38c5869
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-02-13 13:29:11 +0800

    1 file modified

--- midterms/myfirstapp.js
+++ midterms/myfirstapp.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('./', function (request, response){
-  response.send("HELLO WORLD");
-})
+app.get('/', function (request, response) {
+  response.send('HELLO WORLD');
+});
 
-app.listen(port, () => {
-  console.log("Example app listening at http://localhost:3000");
+app.listen(3000, function () {
+  console.log('Example app listening at http://localhost:3000');
 });
