b12a03f34d846a01ff8bd2c4a63265a7e6352667
Knnthh <118255003+Knnthh@users.noreply.github.com>
2023-03-22 16:04:53 +0800

    1 file modified

--- midterms/Midterm-Exam/app.js
+++ midterms/Midterm-Exam/app.js
@@ -7,12 +7,10 @@ const dishes = [
     {type: 'bagnet', province: 'ilocos', price: 370}
 ];
 
-// Route to display all records
 app.get('/dishes', (req, res) => {
     res.json(dishes);
 });
 
-// Route to display a specific record based on the dish type
 app.get('/dishes/:type', (req, res) => {
     const dish = dishes.find(d => d.type === req.params.type);
     if (!dish) {
@@ -22,12 +20,10 @@ app.get('/dishes/:type', (req, res) => {
     }
 });
 
-// Route to handle undefined routes
 app.use((req, res) => {
     res.status(404).json({error: 'Record not found'});
 });
 
-// Start the server
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
